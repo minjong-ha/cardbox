c3cfe151143822d6c0a983ea0e45ab74e1bea12a
하민종 <haminjong@haminjong-ui-MacBookAir.local>
2022-02-12 12:45:36 +0900

    DONE: set the app name to 'CardBox'; TODO: complete setup

---
+++ CardBox/CardBox/CardBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardBoxApp.swift
+//  CardBox
+//
+//  Created by 하민종 on 2022/02/12.
+//
+
+import SwiftUI
+
+@main
+struct CardBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
