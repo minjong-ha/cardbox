c4817476c71f8df23489e69710ef8a5923858456
minjong-ha <hmjong1128@gmail.com>
2022-02-27 22:14:22 +0900

    DONE: get location from latitude and longitude test code; TODO: complete location auto-fill

--- CardBox/CardBox/AddNewCardView.swift
+++ CardBox/CardBox/AddNewCardView.swift
@@ -155,6 +155,14 @@ struct AddNewCardView: View {
             let longitude = CLLocationManager().location?.coordinate.longitude
             self.location = "\(latitude) \(longitude)"
             
+            let findLocation = CLLocation(latitude: 37.576029, longitude: 126.976920)
+            let geocoder = CLGeocoder()
+            let locale = Locale(identifier: "Ko-kr") //원하는 언어의 나라 코드를 넣어주시면 됩니다.
+            geocoder.reverseGeocodeLocation(findLocation, preferredLocale: locale, completionHandler: {(placemarks, error) in
+                if let address: [CLPlacemark] = placemarks {
+                    if let name: String = address.last?.name { print(name) } //전체 주소
+                }
+            })
         }
         .onDisappear(perform:  {
             print("DEBUG: AddNewCard View onDisappear")
