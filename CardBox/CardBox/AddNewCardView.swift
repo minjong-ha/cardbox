3f9673e4384ee8900b4698d42d2a77e1550286fd
minjong-ha <hmjong1128@gmail.com>
2022-03-05 17:32:21 +0900

    DONE: do minor adaptations; TODO: solve keyboard issues

--- CardBox/CardBox/AddNewCardView.swift
+++ CardBox/CardBox/AddNewCardView.swift
@@ -58,13 +58,87 @@ struct AddNewCardView: View {
     @State var currentDate = Date.now
     @State var tagList: Array<String> = []
     let realm = try! Realm()
+    
+    @State var encryptedPassword = ""
+    
+    @State var isTitleExist: Bool = false
+    @State var isTagExist: Bool = false
+    @State var isPasswordExist: Bool = false
     //=====
     
     init() {
         print("DEBUG: load AddNewCardView")
         print("DEBUG: ", Realm.Configuration.defaultConfiguration.fileURL)
+    }
+    
+    private func onAppearUpdate() {
+        let today = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
         
+        print("DEBUG: AddCardView onAppear()")
+        self.date = dateFormatter.string(from: self.currentDate)
+        //self.date = dateFormatter.string(from: today)
         
+        //=====
+        let cardInfoList = realm.objects(CardInfo.self)
+        
+        if (cardInfoList.count > 0) {
+            for cardInfo in cardInfoList {
+                let card = realm.object(ofType: Card.self, forPrimaryKey: cardInfo.cardUUID)
+                let cardTag : String = card!.cardTag
+                let cardTitle : String = card!.cardTitle
+                
+                print("DEBUG: ", cardTag, cardTitle)
+                if (!self.tagList.contains(cardTag)) {
+                    self.tagList.append(cardTag)
+                }
+            }
+        }
+        print(self.tagList.count)
+        //=====
+    }
+    
+    private func realmUpdateCard() {
+        let realm = try! Realm()
+        
+        let card = Card()
+        let cardInfo = CardInfo()
+        
+        let uuid = NSUUID().uuidString
+        self.uuid = uuid
+        
+        card.cardUUID = self.uuid
+        card.cardTitle = self.title
+        card.cardTag = self.tag
+        card.cardLocation = self.location
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        self.date = dateFormatter.string(from: self.currentDate)
+        card.cardDate = self.date
+        
+        card.cardContents = self.contents
+        
+        cardInfo.cardUUID = self.uuid
+        cardInfo.isPrivate = false
+        cardInfo.isEncrypt = self.isEncrypt
+        cardInfo.isCloud = self.isCloud
+        cardInfo.isChecked = self.isChecked
+        
+        //TODO: what if the data is empty(nil)? + make it module in Card and Authority classes
+        try! realm.write {
+            realm.add(card, update: .modified)
+            realm.add(cardInfo, update: .modified)
+        }
+        
+        print("DEBUG: Add New Card! Button Action")
+        print("DEBUG:", self.uuid)
+        print("DEBUG:", self.title)
+        print("DEBUG:", self.tag)
+        print("DEBUG:", self.location)
+        print("DEBUG:", self.date)
+        print("DEBUG:", self.contents)
     }
     
     func setLocation() {
@@ -100,12 +174,8 @@ struct AddNewCardView: View {
                 
                 HStack(alignment: .center) {
                     VStack(alignment: .leading) {
-                        //TODO: Dropdown with Menu{} in SwiftUI
-                        //TODO: add Tag to Card RealmObject first
                         Text("Tag")
                             .bold()
-                        //TextField("Tag", text: $tag)
-                            //.textFieldStyle(.roundedBorder)
                         Menu {
                             ForEach(self.tagList, id: \.self) { cardTag in
                                 Button(action: {
@@ -115,8 +185,6 @@ struct AddNewCardView: View {
                                 }
                             }
                             Button(action: {
-                                //REFERENCE: UIHOSTINGCONTROLLER is very simillar with mail app write new mail
-                                //TODO: alert with textfield
                                 let alertController = UIAlertController(title: "Add New Tag", message: "You can add a new tag", preferredStyle: .alert)
                                 
                                 alertController.addTextField { (textField : UITextField!) -> Void in
@@ -125,13 +193,7 @@ struct AddNewCardView: View {
                                 
                                 let saveAction = UIAlertAction(title: "Save", style: .default, handler: { alert -> Void in
                                     let secondTextField = alertController.textFields![0] as UITextField
-                                    if (secondTextField.text == "") {
-                                        Alert(title: Text("Alert"), message: Text("Tag should not be empty!"), dismissButton: .cancel())
-                                    }
-                                    else {
-                                        self.tag = secondTextField.text!
-                                        print("county code :  \(secondTextField.text!)")
-                                    }
+                                    self.tag = secondTextField.text!
                                 })
                                 
                                 let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil )
@@ -151,17 +213,12 @@ struct AddNewCardView: View {
                     }
                     
                     VStack(alignment: .leading) {
-                        //TODO: replace it to the datepicker!
-						//TODO: https://developer.apple.com/forums/thread/126990
-                        //TODO: https://stackoverflow.com/questions/56489107/using-swiftui-how-can-i-add-datepicker-only-while-textfield-is-in-editing-mode
                         Text("Date")
                             .bold()
                         HStack(alignment: .center) {
                             DatePicker("", selection: $currentDate, displayedComponents: .date)
                             DatePicker("", selection: $currentDate, displayedComponents: .hourAndMinute)
                         }
-                        //TextField("Date", text: $date)
-                         //   .textFieldStyle(.roundedBorder)
                     }
                 }
                 
@@ -204,6 +261,7 @@ struct AddNewCardView: View {
 					//TODO: move textfield up when the keyboard pop (https://stackoverflow.com/questions/56491881/move-textfield-up-when-the-keyboard-has-appeared-in-swiftui)
 					//TODO: add next button for keyboard (https://stackoverflow.com/questions/58673159/how-to-move-to-next-textfield-in-swiftui)
                     //TODO: character limit + TextEditor Background color problem (https://stackoverflow.com/questions/56476007/swiftui-textfield-max-length)
+                    //TODO: Dynamic height textEditor (https://stackoverflow.com/questions/62620613/dynamic-row-hight-containing-texteditor-inside-a-list-in-swiftui)
                     //(https://stackoverflow.com/questions/65459579/texteditor-added-swiftui)
                     //default text: (https://stackoverflow.com/questions/62741851/how-to-add-placeholder-text-to-texteditor-in-swiftui) // ZStack
                     Text("Contents")
@@ -215,73 +273,106 @@ struct AddNewCardView: View {
                         .frame(width: (UIScreen.main.bounds.size.width * 0.9))
                 }
                 
-                //TODO: add help icon and description
                 VStack (alignment: .leading) {
-                    Toggle("Private?", isOn: $isPrivate)
-                    Toggle("Encrypted?", isOn: $isEncrypt)
-                        .tint(.yellow)
-                    Toggle("Cloud?", isOn: $isCloud)
-                        .tint(.blue)
-                    Toggle("Checked?", isOn: $isChecked)
-                        .tint(.orange)
+                    HStack (alignment: .center) {
+                        Toggle("Private?", isOn: $isPrivate)
+                            .onTapGesture {
+                                withAnimation {
+                                    self.isPrivate.toggle()
+                                }
+                            }
+                        
+                        Button (action: {
+                            let alertController = UIAlertController(title: "Private?", message: "If the 'Private' is active,\nthe card will be located in PrivateBox, not PublicBox", preferredStyle: .alert)
+                            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil )
+                            alertController.addAction(cancelAction)
+                            
+                            UIApplication.shared.windows.first?.rootViewController?.present(alertController, animated: true, completion: nil)
+                        }) {
+                            Image(systemName: "questionmark.circle")
+                        }
+                    }
+                    if (self.isPrivate) {
+                        HStack (alignment: .center) {
+                            Toggle("Encrypted?", isOn: $isEncrypt)
+                                .onTapGesture {
+                                    withAnimation {
+                                        self.isEncrypt.toggle()
+                                    }
+                                }
+                                .tint(.yellow)
+                                .opacity(self.isPrivate ? 1 : 0)
+                                .transition(.slide)
+                            Button (action: {
+                                let alertController = UIAlertController(title: "Encrypted?", message: "If the 'Encrypt' is active,\nthe card will be encrypted with the 'Password'", preferredStyle: .alert)
+                                let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil )
+                                alertController.addAction(cancelAction)
+                                
+                                UIApplication.shared.windows.first?.rootViewController?.present(alertController, animated: true, completion: nil)
+                            }) {
+                                Image(systemName: "questionmark.circle")
+                            }
+                        }
+                    }
+                    if (self.isEncrypt) {
+                        VStack(alignment: .leading) {
+                            Text("Password")
+                            SecureField("Enter a Password", text: $encryptedPassword)
+                                .opacity(self.isEncrypt ? 1 : 0)
+                                .transition(.scale)
+                        }
+                    }
+                    HStack (alignment: .center) {
+                        Toggle("Cloud?", isOn: $isCloud)
+                            .tint(.blue)
+                        Button (action: {
+                            let alertController = UIAlertController(title: "Cloud?", message: "If the 'Cloud' is active,\nthe card will be backup automatically in the iCloud", preferredStyle: .alert)
+                            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil )
+                            alertController.addAction(cancelAction)
+                            
+                            UIApplication.shared.windows.first?.rootViewController?.present(alertController, animated: true, completion: nil)
+                        }) {
+                            Image(systemName: "questionmark.circle")
+                        }
+                    }
+                    HStack (alignment: .center) {
+                        Toggle("Checked?", isOn: $isChecked)
+                            .tint(.orange)
+                        Button (action: {
+                            let alertController = UIAlertController(title: "Checked?", message: "If the 'Checked is active,\nthe card will be lined in the Box", preferredStyle: .alert)
+                            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil )
+                            alertController.addAction(cancelAction)
+                            
+                            UIApplication.shared.windows.first?.rootViewController?.present(alertController, animated: true, completion: nil)
+                        }) {
+                            Image(systemName: "questionmark.circle")
+                        }
+                    }
                 }
             }
         }
         .navigationTitle("Add a new Card")
         .toolbar {
             ToolbarItemGroup(placement: .navigationBarTrailing) {
-                
                 Button(action: {
-                    if (self.tag.isEmpty) {
-                        let alertController = UIAlertController(title: "Warning", message: "Tag should not be empty!!!", preferredStyle: .alert)
+                    if (!self.tag.isEmpty) { self.isTagExist = true }
+                    if (!self.title.isEmpty) { self.isTitleExist = true }
+                    if (self.isEncrypt) {
+                        if (!self.encryptedPassword.isEmpty) { self.isPasswordExist = true }
+                    }
+                    else { self.isPasswordExist = true }
+                    
+                    if (!self.isTagExist || !self.isTitleExist || !self.isPasswordExist) {
+                        let alertController = UIAlertController(title: "Warning!\nThere are empty fields!", message: "Title, Tag, and Password(optional)\nshould not be empty!", preferredStyle: .alert)
                         let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil )
                         alertController.addAction(cancelAction)
                         
                         UIApplication.shared.windows.first?.rootViewController?.present(alertController, animated: true, completion: nil)
                     }
                     else {
-                        let realm = try! Realm()
+                        self.realmUpdateCard()
                         
-                        let card = Card()
-                        let cardInfo = CardInfo()
-                        
-                        let uuid = NSUUID().uuidString
-                        self.uuid = uuid
-                        
-                        card.cardUUID = self.uuid
-                        card.cardTitle = self.title
-                        card.cardTag = self.tag
-                        card.cardLocation = self.location
-                        
-                        let dateFormatter = DateFormatter()
-                        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
-                        self.date = dateFormatter.string(from: self.currentDate)
-                        card.cardDate = self.date
-                        
-                        card.cardContents = self.contents
-                        
-                        cardInfo.cardUUID = self.uuid
-                        //cardInfo.isPrivate = self.isPrivate
-                        cardInfo.isPrivate = false
-                        cardInfo.isEncrypt = self.isEncrypt
-                        cardInfo.isCloud = self.isCloud
-                        cardInfo.isChecked = self.isChecked
-                        
-                        //TODO: what if the data is empty(nil)? + make it module in Card and Authority classes
-                        try! realm.write {
-                            realm.add(card, update: .modified)
-                            realm.add(cardInfo, update: .modified)
-                        }
-                        
-                        print("DEBUG: Add New Card! Button Action")
-                        print("DEBUG:", self.uuid)
-                        print("DEBUG:", self.title)
-                        print("DEBUG:", self.tag)
-                        print("DEBUG:", self.location)
-                        print("DEBUG:", self.date)
-                        print("DEBUG:", self.contents)
-                        
-                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        //UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
                         self.dismiss()
                     }
                 }) {
@@ -290,38 +381,8 @@ struct AddNewCardView: View {
             }
         }
         .onAppear {
-            let today = Date()
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
-            
-            print("DEBUG: AddCardView onAppear()")
-            self.date = dateFormatter.string(from: self.currentDate)
-            //self.date = dateFormatter.string(from: today)
-            
-            //=====
-            let cardInfoList = realm.objects(CardInfo.self)
-            
-            if (cardInfoList.count > 0) {
-                for cardInfo in cardInfoList {
-                    let card = realm.object(ofType: Card.self, forPrimaryKey: cardInfo.cardUUID)
-                    let cardTag : String = card!.cardTag
-                    let cardTitle : String = card!.cardTitle
-                    
-                    print("DEBUG: ", cardTag, cardTitle)
-                    if (!self.tagList.contains(cardTag)) {
-                        self.tagList.append(cardTag)
-                    }
-                }
-            }
-            print(self.tagList.count)
-            //=====
+            self.onAppearUpdate()
         }
-        /*
-        .onDisappear(perform:  {
-            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-            print("DEBUG: AddNewCard View onDisappear")
-        })
-        */
     }
   }
 
