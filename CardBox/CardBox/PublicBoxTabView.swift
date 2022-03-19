69947d0f5c2229c43f128d938b704d09450b73d0
minjong-ha <hmjong1128@gmail.com>
2022-03-19 20:35:35 +0900

    DONE: fix onDelete bug; TODO: new issue... onDelete animation error...

--- CardBox/CardBox/PublicBoxTabView.swift
+++ CardBox/CardBox/PublicBoxTabView.swift
@@ -14,17 +14,16 @@ import RealmSwift
 
 struct PublicBoxTabView: View {
     
-    let realm = try! Realm()
+    @State private var sectionList: Array<SectionCell> = []
     
-    @State var isPublicExist: Bool = false
-    @State var publicCardCellList: Array<CardCell> = []
-	@State var publicInfoList: Array<CardInfoCell> = []
+    private let realm = try! Realm()
     
-    @State var isEditing = false
+    @State private var isPublicExist: Bool = false
+    @State private var tagList: Array<String> = []
+    
+    @State private var isEditing = false
     @State private var searchText: String = ""
     @State private var searchTag: String = ""
-    
-    @State var tagList: Array<String> = []
     
     @FocusState private var isFocused: Bool
     
@@ -48,18 +47,39 @@ struct PublicBoxTabView: View {
         //if (self.publicCardList.count > 0) {
         if (publicCardInfoList.count > 0) {
             self.isPublicExist = true
-            self.publicCardCellList.removeAll()
-            self.tagList.removeAll()
+            self.sectionList.removeAll()
             
             for publicCardInfo in publicCardInfoList {
+                
                 let publicCard = realm.object(ofType: Card.self, forPrimaryKey: publicCardInfo.cardUUID)
                 let publicInfoCell = CardInfoCell.init(cardUUID: publicCard!.cardUUID, isPrivate: publicCardInfo.isPrivate, isEncrypt: publicCardInfo.isEncrypt, isCloud: publicCardInfo.isCloud, isChecked: publicCardInfo.isChecked)
                 let publicCardCell = CardCell.init(cardUUID: publicCard!.cardUUID, cardTag: publicCard!.cardTag, cardTitle: publicCard!.cardTitle, cardLocation: publicCard!.cardLocation, cardDate: publicCard!.cardDate, cardContents: publicCard!.cardContents, cardInfo: publicInfoCell)
                 let cardTag : String = publicCard!.cardTag
                 
-                self.publicCardCellList.append(publicCardCell)
-                if (!self.tagList.contains(cardTag)) {
-                    self.tagList.append(cardTag)
+                var isExist = false
+                for section in self.sectionList {
+                    if (section.cardTag == cardTag) {
+                        isExist = true
+                    }
+                }
+                if (!isExist) {
+                    var newSection = SectionCell.init()
+                    newSection.cardTag = cardTag
+                    newSection.cardCellList.append(publicCardCell)
+                    self.sectionList.append(newSection)
+                }
+                else {
+                    var isExist = false
+                    let index = self.sectionList.firstIndex(where: { $0.cardTag == cardTag })!
+                    for card in self.sectionList[index].cardCellList {
+                        if (card.cardUUID == publicCardCell.cardUUID) {
+                            isExist = true
+                        }
+                    }
+                    if (!isExist) {
+                        self.sectionList[index].cardCellList.append(publicCardCell)
+                    }
+                    
                 }
             }
         }
@@ -68,26 +88,21 @@ struct PublicBoxTabView: View {
         }
     }
     
-    private func onDeleteCard(at indexSet: IndexSet) {
-    //private func onDeleteCard(at indexSet: IndexSet, in: Int) {
+    private func onDeleteCard(at indexSet: IndexSet, in section: SectionCell) {
         try! realm.write {
-            indexSet.forEach({ index in
-                let publicCardCell = self.publicCardCellList[index]
-                print("DEBUG: try to delete ", index, "cell_", publicCardCell.cardUUID, publicCardCell.cardTag, publicCardCell.cardTitle)
-                
+            indexSet.forEach ({ index in
+                let publicCardCell = section.cardCellList[index]
                 let publicCard = realm.object(ofType: Card.self, forPrimaryKey: publicCardCell.cardUUID)
                 let publicCardInfo = realm.object(ofType: CardInfo.self, forPrimaryKey: publicCardCell.cardUUID)
                 print("DEBUG: deleting RealmObject ", publicCard!.cardTitle)
-            
-                /*
+                
                 if(publicCard != nil) {
                     realm.delete(publicCard!)
                     realm.delete(publicCardInfo!)
                 }
                 else {
-                    print("DEBUG: There is no matched RealmObject")
+                    print("DEBUG: There is no matched RealmObject!")
                 }
-                */
             })
         }
         self.onAppearUpdate()
@@ -98,32 +113,30 @@ struct PublicBoxTabView: View {
             ZStack() {
                 VStack {
                     List {
-                        ForEach(self.tagList, id: \.self) { section in
-                            Section(header: Text(section).bold().font(.title3), content:  {
-                                let sectionCardCellList = self.publicCardCellList.filter { card in
-                                    return card.cardTag == section
-                                }
-                                if (sectionCardCellList.count > 0) {
-                                    ForEach(sectionCardCellList, id: \.self) { publicCardCell in
-                                        if (self.searchText == "") {
+                        ForEach (self.sectionList, id: \.self) { section in
+                            Section(header: Text(section.cardTag).bold().font(.title3), content:  {
+                                ForEach(section.cardCellList, id: \.self) { publicCardCell in
+                                    if (self.searchText == "") {
+                                        NavigationLink(destination: OnDemandView(CardView(cardUUID: publicCardCell.cardUUID, localTitle: publicCardCell.cardTitle, localTag: "", localDate: "", localContents: "", localLocation: "", localPrivate: publicCardCell.cardInfo.isPrivate, localEncrypt: publicCardCell.cardInfo.isEncrypt, localCloud: publicCardCell.cardInfo.isCloud, localChecked: publicCardCell.cardInfo.isChecked, isEditState: false))) {
+                                            HStack {
+                                                Label("\(publicCardCell.cardTitle)", systemImage: "envelope.fill")
+                                            }
+                                        }
+                                        
+                                    }
+                                    else {
+                                        if (publicCardCell.cardTitle.contains(self.searchText)) {
                                             NavigationLink(destination: OnDemandView(CardView(cardUUID: publicCardCell.cardUUID, localTitle: publicCardCell.cardTitle, localTag: "", localDate: "", localContents: "", localLocation: "", localPrivate: publicCardCell.cardInfo.isPrivate, localEncrypt: publicCardCell.cardInfo.isEncrypt, localCloud: publicCardCell.cardInfo.isCloud, localChecked: publicCardCell.cardInfo.isChecked, isEditState: false))) {
                                                 HStack {
                                                     Label("\(publicCardCell.cardTitle)", systemImage: "envelope.fill")
                                                 }
                                             }
-                                        }
-                                        else {
-                                            if (publicCardCell.cardTitle.contains(self.searchText)) {
-                                                NavigationLink(destination: OnDemandView(CardView(cardUUID: publicCardCell.cardUUID, localTitle: publicCardCell.cardTitle, localTag: "", localDate: "", localContents: "", localLocation: "", localPrivate: publicCardCell.cardInfo.isPrivate, localEncrypt: publicCardCell.cardInfo.isEncrypt, localCloud: publicCardCell.cardInfo.isCloud, localChecked: publicCardCell.cardInfo.isChecked, isEditState: false))) {
-                                                    HStack {
-                                                        Label("\(publicCardCell.cardTitle)", systemImage: "envelope.fill")
-                                                    }
-                                                }
-                                                
-                                            }
+                                            
                                         }
                                     }
-                                    .onDelete(perform: self.onDeleteCard)
+                                }
+                                .onDelete {
+                                    self.onDeleteCard(at: $0, in: section)
                                 }
                             })
                         }
