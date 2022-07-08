aba50f3bd856917bcb192bddf06b24b0c56b8753
minjong-ha <hmjong1128@gmail.com>
2022-07-08 09:05:08 +0900

    DONE: add CardListView for Public/Private Box

---
+++ CardBox/CardBox/SubViews/BoxSubViews/CardListView.swift
@@ -0,0 +1,14 @@
+//
+//  CardListView.swift
+//  CardBox
+//
+//  Created by Minjong Ha on 2022/07/04.
+//
+
+import SwiftUI
+
+struct CardListView: View {
+    var body: some View {
+        Text("Listing Cards based on the data that View hands over")
+    }
+}
